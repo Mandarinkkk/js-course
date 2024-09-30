d127faa4ac8dba76c2e07790e819b1c7d7993db9
Mandarinkkk <163579708+Mandarinkkk@users.noreply.github.com>
2024-09-30 22:31:06 +0300

    Update main.js

--- week1/task3/js/main.js
+++ week1/task3/js/main.js
@@ -1,7 +1,12 @@
+const calculate = (l) => { // Функция, которая вычисляет объем икосаэдра по длине ребра.
+  return (((5 * (l ** 3)) * (3 + Math.sqrt(5)))/ 12); 
+  // Вычисление объема по формуле V = (5 * l^3 * (3 + √5)) / 12
+};
+
 const main = () => { // Объявление функции main
   let result = document.getElementById("result"); //Обозначение тега result по его id
   let l = document.getElementById("l").value; //Получение значения из input с id 'l'
-  return (result.innerHTML = (((5 * (l ** 3)) * (3 + Math.sqrt(5)))/ 12)); // Изменение содержимого тега result, расчёт объёма икосаэдра
+  result.innerHTML = calculate(l); // Изменение содержимого тега result
 };
 
 document.getElementById("submit").addEventListener("click", main); // Ожидание нажатия кнопки, при нажатии запуск функции main
