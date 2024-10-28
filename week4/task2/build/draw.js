f4eaddf6f543e56ed26468d3bd53b888947964a6
Mandarinkkk <163579708+Mandarinkkk@users.noreply.github.com>
2024-10-29 02:09:40 +0300

    Delete week4/task2/build directory

--- week4/task2/build/draw.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function drawRectangle(context, x, y, width, height, fillColor) {
-    context.fillStyle = fillColor;
-    context.fillRect(x, y, width, height);
-    context.strokeStyle = 'black';
-    context.strokeRect(x, y, width, height);
-}
-window.drawRectangle = drawRectangle;
-//# sourceMappingURL=draw.js.map
